0e36196aacb8b2d7ea7df512ee99a957af1ff723
Neil Huang <neiltang618@gmail.com>
2023-09-21 00:54:28 +1200

    Update app.js

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.use((err,req,res,next)=>{
     res.render("error", { err })
 })
 
-const port = process.env.PORT || "443"
+const port = process.env.PORT || "3001"
 
 app.listen(port,()=>{
     console.log(`Listening to the port ${port}`);
